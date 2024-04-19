ae7dce71b168f91b38aeeb0db6425f56254eee00
JCox924 <Joshua.Cox@atlasschool.com>
2024-04-19 11:52:21 -0500

    will it push already

--- 7-binary_tree_inorder.c
+++ 7-binary_tree_inorder.c
@@ -19,6 +19,6 @@ void binary_tree_inorder(const binary_tree_t *tree, void (*func)(int))
 	}
 
 	binary_tree_preorder(tree->left, func);
-	func(tree-n);
+	func(tree->n);
 	binary_tree_preorder(tree->right, func);
 }
